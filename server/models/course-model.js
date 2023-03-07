af8355dbb8ec913236f576eed76175911f12a8f4
Weiwei <weiwei30697@gmail.com>
2023-03-07 17:03:46 +0800

    Mern project

---
+++ server/models/course-model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const courseSchema = new Schema({
+  id: { type: String },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  instructor: {
+    //primary key
+    type: mongoose.Schema.Types.ObjectId,
+    //link to "User"
+    ref: "User",
+  },
+  students: {
+    type: [String],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Course", courseSchema);
